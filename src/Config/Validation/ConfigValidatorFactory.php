wikimedia/mediawiki-extensions-AutoModerator

commit e2afd006be5fa84407f99937770a63d6c96967f6

remove $wgAutoModeratorWikiConfigPageTitle
There is no need for a configurable wiki config page name

Change-Id: I5ae47b436fd46f483c8ead1720c3dca667533731

src/Config/Validation/ConfigValidatorFactory.php
@@ -3,13 +3,11 @@
 namespace AutoModerator\Config\Validation;
 
 use InvalidArgumentException;
-use MediaWiki\Config\Config;
 use MediaWiki\Linker\LinkTarget;
 use MediaWiki\Title\Title;
 use MediaWiki\Title\TitleFactory;
 
 class ConfigValidatorFactory {
-	private Config $config;
 	private TitleFactory $titleFactory;
 
 	/**
@@ -25,28 +23,12 @@ class ConfigValidatorFactory {
 	];
 
 	/**
-	 * @param Config $config
 	 * @param TitleFactory $titleFactory
 	 */
 	public function __construct(
-		Config $config,
 		TitleFactory $titleFactory
 	) {
-		$this->config = $config;
 		$this->titleFactory = $titleFactory;
-	}
-
-	/**
-	 * Code helper for comparing titles
-	 *
-	 * @param Title $configTitle
-	 * @param string $otherConfigPage
-	 * @return bool
-	 */
-	private function titleEquals( Title $configTitle, string $otherConfigPage ): bool {
-		$varTitle = $this->titleFactory
-			->newFromText( $otherConfigPage );
-		return $varTitle !== null && $configTitle->equals( $varTitle );
 	}
 
 	/**
@@ -55,16 +37,12 @@ class ConfigValidatorFactory {
 	 * @return Title[]
 	 */
 	public function getSupportedConfigPages(): array {
-		return array_filter(
-			array_map(
-				function ( string $var ) {
-					return $this->titleFactory->newFromText(
-						$this->config->get( $var )
-					);
-				},
-				array_keys( self::CONFIG_VALIDATOR_MAP )
-			)
+		// Update this when CONFIG_VALIDATOR_MAP has another entry
+		$title = $this->titleFactory->makeTitle(
+		 NS_MEDIAWIKI,
+		 'AutoModeratorConfig.js'
 		);
+		return [ $title ];
 	}
 
 	/**
@@ -91,18 +69,13 @@ class ConfigValidatorFactory {
 	 *
 	 * @param LinkTarget $configPage
 	 * @return IConfigValidator
-	 * @throws InvalidArgumentException when passed config page is not recognized; this should
+	 * @throws InvalidArgumentException when no config page is passed; this should
 	 * never happen in practice.
 	 */
 	public function newConfigValidator( LinkTarget $configPage ): IConfigValidator {
 		$title = $this->titleFactory->newFromLinkTarget( $configPage );
 
-		foreach ( self::CONFIG_VALIDATOR_MAP as $var => $validatorClass ) {
-			if ( $this->titleEquals( $title, $this->config->get( $var ) ) ) {
-				return $this->constructValidator( $validatorClass );
-			}
-		}
-
-		throw new InvalidArgumentException( 'Unsupported config page' );
+		$validatorClass = array_values( self::CONFIG_VALIDATOR_MAP )[ 0 ];
+		return $this->constructValidator( $validatorClass );
 	}
 }

src/Config/Validation/AutoModeratorConfigValidation.php
@@ -13,12 +13,6 @@ use StatusValue;
 class AutoModeratorConfigValidation implements IConfigValidator {
 	use DatatypeValidationTrait;
 
-	/**
-	 * Copy of TemplateCollectionFeature::MAX_TEMPLATES_IN_COLLECTION. We avoid a direct reference
-	 * to keep CirrusSearch an optional dependency.
-	 */
-	public const MAX_TEMPLATES_IN_COLLECTION = 800;
-
 	private function getConfigDescriptors(): array {
 		return [
 			'AutoModeratorEnableRevisionCheck' => [

src/Config/WikiPageConfig.php
@@ -7,6 +7,7 @@ use MediaWiki\Config\ConfigException;
 use MediaWiki\Title\Title;
 use MediaWiki\Title\TitleFactory;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 use StatusValue;
 
 class WikiPageConfig implements Config {
@@ -14,7 +15,6 @@ class WikiPageConfig implements Config {
 	private LoggerInterface $logger;
 	private TitleFactory $titleFactory;
 	private ?WikiPageConfigLoader $configLoader;
-	private ?string $rawConfigTitle;
 	private ?Title $configTitle = null;
 	/**
 	 * @var bool Hack to disable DB access in non-database tests.
@@ -25,20 +25,17 @@ class WikiPageConfig implements Config {
 	 * @param LoggerInterface $logger
 	 * @param TitleFactory $titleFactory
 	 * @param WikiPageConfigLoader $configLoader
-	 * @param string $rawConfigTitle
 	 * @param bool $isTestWithStorageDisabled
 	 */
 	public function __construct(
 		LoggerInterface $logger,
 		TitleFactory $titleFactory,
 		WikiPageConfigLoader $configLoader,
-		string $rawConfigTitle,
 		bool $isTestWithStorageDisabled
 	) {
 		$this->logger = $logger;
 		$this->titleFactory = $titleFactory;
 		$this->configLoader = $configLoader;
-		$this->rawConfigTitle = $rawConfigTitle;
 		$this->isTestWithStorageDisabled = $isTestWithStorageDisabled;
 	}
 
@@ -49,18 +46,20 @@ class WikiPageConfig implements Config {
 	 * Title (which may talk to the DB) until whenever config is first fetched,
 	 * which should be much later, and probably after init sequence finished.
 	 *
-	 * @throws ConfigException
+	 * @throws RuntimeException
 	 * @return Title
 	 */
 	private function getConfigTitle(): Title {
 		if ( $this->configTitle == null ) {
-			$configTitle = $this->titleFactory->newFromText( $this->rawConfigTitle );
-
+			$configTitle = $this->titleFactory->makeTitleSafe(
+				NS_MEDIAWIKI,
+				'AutoModeratorConfig.json'
+			);
 			if (
 				$configTitle === null ||
 				!$configTitle->isSiteJsonConfigPage()
 			) {
-				throw new ConfigException( 'Invalid AutoModeratorWikiConfigPageTitle' );
+				throw new RuntimeException( 'Invalid AutoModeratorWikiConfigPageTitle' );
 			}
 
 			$this->configTitle = $configTitle;

src/ServiceWiring.php
@@ -28,7 +28,6 @@ return [
 	): ConfigValidatorFactory {
 		$autoModeratorServices = AutoModeratorServices::wrap( $services );
 		return new ConfigValidatorFactory(
-			$services->getMainConfig(),
 			$services->getTitleFactory()
 		);
 	},
@@ -39,7 +38,6 @@ return [
 			LoggerFactory::getInstance( 'AutoModerator' ),
 			$services->getTitleFactory(),
 			$autoModeratorServices->getWikiPageConfigLoader(),
-			$services->getMainConfig()->get( 'AutoModeratorWikiConfigPageTitle' ),
 			defined( 'MW_PHPUNIT_TEST' ) && $services->isStorageDisabled()
 		);
 	},

commit 7daa69ed5b4a08d2099de13ace26b2c5d43a5547

Fix Validator regression
regression introduced in e2afd006be5fa84407f99937770a63d6c96967f6
caused config page validators to not run

Bug: T363691
Change-Id: I983879ab60890b96768a33b68dc07b83918afe8f

src/Config/Validation/ConfigValidatorFactory.php
@@ -19,7 +19,7 @@ class ConfigValidatorFactory {
 	 * as well.
 	 */
 	private const CONFIG_VALIDATOR_MAP = [
-		'AutoModeratorWikiConfigPageTitle' => AutoModeratorConfigValidation::class
+		'AutoModeratorConfig.json' => AutoModeratorConfigValidation::class
 	];
 
 	/**
@@ -32,17 +32,35 @@ class ConfigValidatorFactory {
 	}
 
 	/**
+	 * Code helper for comparing titles
+	 *
+	 * @param Title $configTitle
+	 * @param string $otherConfigPage
+	 * @return bool
+	 */
+	private function titleEquals( Title $configTitle, string $otherConfigPage ): bool {
+		$varTitle = $this->titleFactory
+			->makeTitleSafe( NS_MEDIAWIKI, $otherConfigPage );
+		return $varTitle !== null && $configTitle->equals( $varTitle );
+	}
+
+	/**
 	 * Return list of supported config pages
 	 *
 	 * @return Title[]
 	 */
 	public function getSupportedConfigPages(): array {
-		// Update this when CONFIG_VALIDATOR_MAP has another entry
-		$title = $this->titleFactory->makeTitle(
-		 NS_MEDIAWIKI,
-		 'AutoModeratorConfig.js'
+		return array_filter(
+			array_map(
+				function ( string $var ) {
+					return $this->titleFactory->makeTitleSafe(
+						NS_MEDIAWIKI,
+						$var
+					);
+				},
+				array_keys( self::CONFIG_VALIDATOR_MAP )
+			)
 		);
-		return [ $title ];
 	}
 
 	/**
@@ -69,13 +87,18 @@ class ConfigValidatorFactory {
 	 *
 	 * @param LinkTarget $configPage
 	 * @return IConfigValidator
-	 * @throws InvalidArgumentException when no config page is passed; this should
+	 * @throws InvalidArgumentException when passed config page is not recognized; this should
 	 * never happen in practice.
 	 */
 	public function newConfigValidator( LinkTarget $configPage ): IConfigValidator {
 		$title = $this->titleFactory->newFromLinkTarget( $configPage );
 
-		$validatorClass = array_values( self::CONFIG_VALIDATOR_MAP )[ 0 ];
-		return $this->constructValidator( $validatorClass );
+		foreach ( self::CONFIG_VALIDATOR_MAP as $var => $validatorClass ) {
+			if ( $this->titleEquals( $title, $var ) ) {
+				return $this->constructValidator( $validatorClass );
+			}
+		}
+
+		throw new InvalidArgumentException( 'Unsupported config page' );
 	}
 }